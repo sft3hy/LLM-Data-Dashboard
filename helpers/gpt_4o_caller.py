bd0b2a650d0a7ec1268688f916a6800c48a066ab
Sam Townsend <sft3hy@virginia.edu>
2024-12-21 22:55:03 -0500

    caps

--- helpers/gpt_4o_caller.py
+++ helpers/gpt_4o_caller.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI
 
-token = os.environ["gh_access_token"]
+token = os.environ["GH_ACCESS_TOKEN"]
 endpoint = "https://models.inference.ai.azure.com"
 model_name = "gpt-4o"
 
