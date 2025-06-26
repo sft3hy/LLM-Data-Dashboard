ca32620331e9127897a3eea6c27dd805238993ad
Samuel Townsend <sft3hy@virginia.edu>
2025-06-26 09:40:18 -0400

    Update kaggle_utils.py

--- utils/kaggle_utils.py
+++ utils/kaggle_utils.py
@@ -2,23 +2,25 @@ import os
 import kaggle
 from kaggle.api.kaggle_api_extended import KaggleApi
 from config import KAGGLE_API
-    
 
-
-def search_datasets(query, sort_by='hottest', size=6):
+def search_datasets(query, sort_by='relevance', size=6):
     """
     Searches Kaggle datasets based on a query.
 
     Parameters:
-        api (KaggleApi): Authenticated Kaggle API object.
-        query (str): Search term to query datasets.
-        sort_by (str): Sort by criteria ('relevance', 'votes', 'updated', 'active').
-        size (int): Limit the number of results (default is None, returns all).
-    
+    api (KaggleApi): Authenticated Kaggle API object.
+    query (str): Search term to query datasets.
+    sort_by (str): Sort by criteria ('relevance', 'votes', 'updated', 'active').
+    size (int): Limit the number of results (default is 6).
+
     Returns:
-        list: A list of datasets matching the query.
+    list: A list of datasets matching the query.
     """
     try:
+        # Valid sort_by values are: 'hottest', 'newest', 'updated', 'votes'
+        if sort_by not in ['hottest', 'newest', 'updated', 'votes']:
+            sort_by = 'relevance'
+
         datasets = KAGGLE_API.dataset_list(search=query, sort_by=sort_by)
         if size:
             datasets = datasets[:size]
@@ -31,10 +33,10 @@ def download_dataset(dataset_slug, download_path='datasets', unzip=True):
     Downloads a dataset from Kaggle.
 
     Parameters:
-        api (KaggleApi): Authenticated Kaggle API object.
-        dataset_slug (str): Slug of the dataset (e.g., 'zillow/zecon').
-        download_path (str): Path to download the dataset (default is './datasets').
-        unzip (bool): Whether to unzip the downloaded dataset (default is True).
+    api (KaggleApi): Authenticated Kaggle API object.
+    dataset_slug (str): Slug of the dataset (e.g., 'zillow/zecon').
+    download_path (str): Path to download the dataset (default is './datasets').
+    unzip (bool): Whether to unzip the downloaded dataset (default is True).
     """
     try:
         os.makedirs(download_path, exist_ok=True)
